e810adfa9ff79284ce06f56e93c060df501a42d3
Todd Nguyen <todd.nguyen47@gmail.com>
2022-05-07 01:56:24 -0700

    Update `listMultithread` worker to do work on a single element instead

--- src/main/java/com/github/toddnguyen47/listmultithread/ListMultithread.java
+++ src/main/java/com/github/toddnguyen47/listmultithread/ListMultithread.java
@@ -10,50 +10,68 @@ import java.util.concurrent.Future;
 
 public class ListMultithread<T> {
 
-    private final List<Worker<T>> workers;
-    private final int numThreads;
+  private final Worker<T> worker;
+  private final int numThreads;
 
-    public ListMultithread(final List<Worker<T>> workers, final int numThreads) {
-        assert workers.size() == numThreads : "List of workers must be the same as `numThreads`!";
-        this.workers = workers;
-        this.numThreads = numThreads;
+  public ListMultithread(Worker<T> worker, final int numThreads) {
+    this.worker = worker;
+    this.numThreads = numThreads;
+  }
+
+  public void runMultithread(final List<T> inputList) throws InterruptedException, ExecutionException {
+    final ExecutorService exec = Executors.newFixedThreadPool(numThreads);
+    final List<Future<?>> futures = new ArrayList<>();
+
+    for (final T elem : inputList) {
+      final Future<?> future = exec.submit(() -> {
+        worker.run(elem);
+      });
+      futures.add(future);
     }
 
-    /**
-     * <div> Run this service in multithreaded fashion. </div> <div> Ref:
-     * https://stackoverflow.com/a/30655678/6323360 </div>
-     *
-     * @param inputList
-     * @param client
-     * @throws InterruptedException
-     * @throws ExecutionException
-     */
-    public void runMultithread(final List<T> inputList) throws InterruptedException, ExecutionException {
-
-        final int listSize = inputList.size();
-        final ExecutorService exec = Executors.newFixedThreadPool(numThreads);
-        // Ex: With 14 items, we want [4, 4, 3, 3] items instead of [3, 3, 3, 5] items
-        final int minItemsPerThread = listSize / numThreads;
-        final int maxItemsPerThread = minItemsPerThread + 1;
-        final int threadIndicesWithMaxItems = listSize - (minItemsPerThread * numThreads);
-        final List<Future<?>> futures = new ArrayList<>(listSize);
-        int startIndex = 0;
-        for (int i = 0; i < numThreads; i++) {
-            final int itemCount = i < threadIndicesWithMaxItems ? maxItemsPerThread : minItemsPerThread;
-            final int endIndex = startIndex + itemCount;
-            final List<T> subList = Collections.synchronizedList(inputList.subList(startIndex, endIndex));
-            final Worker<T> worker = workers.get(i);
-            futures.add(exec.submit(() -> {
-                worker.run(subList);
-            }));
-            startIndex = endIndex;
-        }
-
-        for (final Future<?> future : futures) {
-            future.get();
-        }
-
-        // SHUT DOWN exec. THIS MUST BE after the `get()` call!
-        exec.shutdown();
+    for (final Future<?> future : futures) {
+      future.get();
     }
+
+    // SHUT DOWN exec. THIS MUST BE after the `get()` call!
+    exec.shutdown();
+  }
+
+  /**
+   * <div> Run this service in multithreaded fashion. </div> <div> Ref:
+   * https://stackoverflow.com/a/30655678/6323360 </div>
+   *
+   * @deprecated
+   * @param inputList
+   * @param client
+   * @throws InterruptedException
+   * @throws ExecutionException
+   */
+  public void runMultithreadOld(final List<T> inputList) throws InterruptedException, ExecutionException {
+
+    final int listSize = inputList.size();
+    final ExecutorService exec = Executors.newFixedThreadPool(numThreads);
+    // Ex: With 14 items, we want [4, 4, 3, 3] items instead of [3, 3, 3, 5] items
+    final int minItemsPerThread = listSize / numThreads;
+    final int maxItemsPerThread = minItemsPerThread + 1;
+    final int threadIndicesWithMaxItems = listSize - (minItemsPerThread * numThreads);
+    final List<Future<?>> futures = new ArrayList<>();
+    int startIndex = 0;
+    for (int i = 0; i < numThreads; i++) {
+      final int itemCount = i < threadIndicesWithMaxItems ? maxItemsPerThread : minItemsPerThread;
+      final int endIndex = startIndex + itemCount;
+      final List<T> subList = Collections.synchronizedList(inputList.subList(startIndex, endIndex));
+      futures.add(exec.submit(() -> {
+        // worker.run(subList);
+      }));
+      startIndex = endIndex;
+    }
+
+    for (final Future<?> future : futures) {
+      future.get();
+    }
+
+    // SHUT DOWN exec. THIS MUST BE after the `get()` call!
+    exec.shutdown();
+  }
 }
--- src/main/java/com/github/toddnguyen47/listmultithread/Worker.java
+++ src/main/java/com/github/toddnguyen47/listmultithread/Worker.java
@@ -1,13 +1,11 @@
 package com.github.toddnguyen47.listmultithread;
 
-import java.util.List;
-
 public interface Worker<T> {
 
-    /**
-     * What the work needs to be done per list.
-     *
-     * @param inputList
-     */
-    void run(final List<T> inputList);
+  /**
+   * What the work needs to be done per list.
+   *
+   * @param inputList
+   */
+  void run(final T elem);
 }
